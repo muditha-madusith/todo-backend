40f45ab333417e7019e32ffcd927b1ec7e1f0cd1
Muditha Madusith <kabmmadusith2003@gmail.com>
2024-02-29 20:53:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,72 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const Todo = require('./models/todo');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Todo = require("./models/todo");
+require("dotenv").config();
 
 const app = express();
-const port =  3001; 
+const port = 3001;
 
 app.use(cors());
 
 //mongodb connect
 const uri = process.env.MONGODB_URL;
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to the Mongodb...")
-}).catch((err) => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the Mongodb...");
+  })
+  .catch((err) => {
     console.log(err);
     process.exit(1); // Exit process if database connection fails
-});
+  });
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get('/api/todos', async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        res.json(todos);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server Error' });
-    }
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
-app.post('/api/todos', async (req, res) => {
-    const { todo } = req.body;
+app.post("/api/todos", async (req, res) => {
+  const { todo } = req.body;
 
-    try {
-        const newTodo = new Todo({
-            todo,
-        });
+  try {
+    const newTodo = new Todo({
+      todo,
+    });
 
-        await newTodo.save();
-        res.status(201).json(newTodo);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server Error' });
-    }
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
-app.delete('/api/todos/:id', async (req, res) => {
-    const { id } = req.params;
+app.delete("/api/todos/:id", async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        await Todo.findByIdAndDelete(id);
-        res.json({ message: 'Todo deleted successfully' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server Error' });
-    }
+  try {
+    await Todo.findByIdAndDelete(id);
+    res.json({ message: "Todo deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
-
